db2c2769612baa7d444b62871b4b47a60187855c
Emiliano Parizzi <eparizzi@gmail.com>
2024-05-16 22:55:27 +0200

    Fix base URL.

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,6 +1,6 @@
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    ? `https://${process.env.NEXT_PUBLIC_BASE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL}`
     : "http://localhost:3000";
 
 const INCLUDES_FORWARD_SLASH_AT_START_REGEX = /^\/(.|\n)*$/;
