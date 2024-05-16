0d3f2228856b5bc39e25007920deb080b36a72e4
Emiliano Parizzi <eparizzi@gmail.com>
2024-05-17 01:11:34 +0200

    Disable Serwist (fails on Vercel).

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
-import withSerwistInit from "@serwist/next";
+// TODO: This is currently failing to build on Vercel
+// import withSerwistInit from "@serwist/next";
 
-const withSerwist = withSerwistInit({
-  // Note: This is only an example. If you use Pages Router,
-  // use something else that works, such as "service-worker/index.ts".
-  swSrc: "src/app/sw.ts",
-  swDest: "public/sw.js",
-});
+// const withSerwist = withSerwistInit({
+//   // Note: This is only an example. If you use Pages Router,
+//   // use something else that works, such as "service-worker/index.ts".
+//   swSrc: "src/app/sw.ts",
+//   swDest: "public/sw.js",
+// });
 
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
@@ -16,4 +17,6 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {};
 
-export default withSerwist(config);
+//export default withSerwist(config);
+
+export default config;
