ccf6b95bdbb652525f9b8ecf5b0a84a9a26eadf6
Emiliano Parizzi <eparizzi@gmail.com>
2024-05-16 21:47:24 +0200

    Remove unused code.

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
-import { postRouter } from "~/server/api/routers/post";
 import { partyRouter } from "./routers/party";
 
 /**
@@ -8,7 +7,6 @@ import { partyRouter } from "./routers/party";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   party: partyRouter,
 });
 
