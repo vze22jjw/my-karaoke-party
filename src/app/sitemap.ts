a6ddf9a4a30fc28ca4075f7a771e9fb774c477dc
Emiliano Parizzi <eparizzi@gmail.com>
2024-05-21 23:20:21 +0200

    Include ToS page in sitemap.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,5 +8,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 1,
     },
+    {
+      url: "https://www.mykaraoke.party/terms-of-service",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
   ];
 }
