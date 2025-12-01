01dbaf1f964df53680ab556b7355d7d4755dbcf0
vze22jjw <vze22jjw@gmail.com>
2025-12-01 14:08:22 -0500

    recude page height

--- src/app/[locale]/join/join-scene.tsx
+++ src/app/[locale]/join/join-scene.tsx
@@ -109,7 +109,8 @@ export default function JoinScene({
 
   return (
     <main className="flex min-h-screen flex-col items-center text-white">
-      <div className="container flex flex-1 flex-col items-center gap-2 px-4 pt-4 pb-12">
+      {/* ADDED justify-center to vertically align the entire block */}
+      <div className="container flex flex-1 flex-col items-center justify-center gap-2 px-4 pt-4 pb-12">
         <Image
           src={logo}
           width={666}
@@ -117,9 +118,9 @@ export default function JoinScene({
           alt="My Karaoke Party logo"
           priority={true}
           placeholder="blur"
-          className="h-auto w-full max-w-[266px] flex-shrink-0"
+          className="h-auto w-full max-w-[133px] flex-shrink-0"
         />
-        <div className="flex w-full max-w-xs flex-1 flex-col items-center justify-center px-5">
+        <div className="flex w-full max-w-xs flex-col items-center px-5 mx-auto">
           {partyName && (
             <div className="mb-4 w-full text-center">
               <p className="text-sm text-white/80">{t('joining')}:</p>
