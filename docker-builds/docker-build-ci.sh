b8ff07973c906fb10c398a6b90eedc8ce486a626
vze22jjw <vze22jjw@gmail.com>
2025-12-06 21:34:00 -0500

    github runner build issues

--- docker-builds/docker-build-ci.sh
+++ docker-builds/docker-build-ci.sh
@@ -6,6 +6,13 @@
 
 set -e
 
+unset HTTP_PROXY
+unset HTTPS_PROXY
+unset http_proxy
+unset https_proxy
+unset NO_PROXY
+unset no_proxy
+
 # Help message
 show_help() {
     echo "Usage: $0 --build-type <type> --platform <arch> [--deploy]"
