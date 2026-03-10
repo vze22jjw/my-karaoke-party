f783bd4ffc2ac037a7214b9f6551822b6b562993
vze22jjw <vze22jjw@gmail.com>
2026-03-10 16:41:17 -0400

    UI updates for up next and current

--- src/components/player-qr-code.tsx
+++ src/components/player-qr-code.tsx
@@ -12,7 +12,8 @@ export function PlayerQrCode({ joinPartyUrl, className }: Props) {
   return (
     <div 
       className={cn(
-        "absolute bottom-28 left-8 z-20 flex items-end", 
+        // Add "w-fit" so the div wraps tightly around the QR code and text
+        "absolute bottom-28 left-8 z-20 flex items-end w-fit pointer-events-none", 
         className
       )}
     >
@@ -21,7 +22,8 @@ export function PlayerQrCode({ joinPartyUrl, className }: Props) {
         href={joinPartyUrl}
         target="_blank"
         rel="noreferrer"
-        className="font-mono text-xl text-white pl-4 text-outline"
+        // Add "pointer-events-auto" so the link remains clickable
+        className="font-mono text-xl text-white pl-4 text-outline pointer-events-auto"
       >
         {joinPartyUrl.split("//")[1]}
       </a>
